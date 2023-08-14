3d62b8dba512997c8d4dc82e65aaa3514fdda6d7
junxiw1kkxi <groovysouldj@gmail.com>
2023-08-14 14:43:30 +0900

    main

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-async function onGAPILoad() {
+const onGAPILoad = async () => {
   await gapi.client.init({
     apiKey: 'AIzaSyB08ejkEhqLAlHYDzSbqkl',
     clientId: '749711979920-2t5ip02556cbojn4u2k7q9ar9e8kjtqh.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/drive.file',
     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
   });
-  
-  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-  
-  updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-}
 
-function updateSignInStatus(isSignedIn) {
+gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+
+updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+};
+
+const updateSignInStatus = (isSignedIn) => {
   if (isSignedIn) {
     $('#signin').hide();
     $('#file-input').show();
@@ -20,12 +20,62 @@ function updateSignInStatus(isSignedIn) {
     $('#signin').show();
     $('#signin').on('click', () => gapi.auth2.getAuthInstance().signIn());
   }
-}
+};
 
-async function handleFileUpload(event) {
+const findOrCreateFolder = async (folderName) => {
+  const query = `mimeType='application/vnd.google-apps.folder' and trashed=false and name='${folderName}'`;
+  let folders;
+
+  try {
+    const response = await gapi.client.drive.files.list({
+      q: query,
+      fields: 'nextPageToken, files(id, name)',
+    });
+
+    folders = response.result.files;
+  } catch (error) {
+    console.error('Error finding folder:', error);
+  }
+
+  let folderId;
+
+  if (folders && folders.length === 0) {
+    try {
+      const metadata = {
+        name: folderName,
+        mimeType: 'application/vnd.google-apps.folder',
+      };
+
+      const response = await gapi.client.drive.files.create({ resource: metadata });
+      folderId = response.result.id;
+
+      console.log(`Created new folder ${folderName} with ID: ${folderId}`);
+    } catch (error) {
+      console.error('Error creating new folder:', error);
+    }
+  }else{
+    folderId = folders[0].id;
+  }
+
+  return folderId;
+};
+
+const handleFileUpload = async (event) => {
   const file = event.target.files[0];
-  const metadata = { name: file.name, mimeType: file.type };
-  
+
+  if (file.type !== 'application/pdf') {
+    alert('PDF 파일만 업로드해 주세요!');
+    return;
+  }
+
+  const folderId = await findOrCreateFolder('PDF_Uploads');
+
+  const metadata = {
+    name: file.name,
+    mimeType: file.type,
+    parents: [folderId],
+  };
+
   try {
     const accessToken = gapi.auth.getToken().access_token;
     const form = new FormData();
@@ -43,6 +93,6 @@ async function handleFileUpload(event) {
   } catch (error) {
     console.error('Error uploading file:', error);
   }
-}
+};
 
 gapi.load('client:auth2', onGAPILoad);
