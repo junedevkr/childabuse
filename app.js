b7e9510913a0f201949cfabaec6a716e7fac112d
junxiw1kkxi <groovysouldj@gmail.com>
2023-08-15 09:45:45 +0900

    main

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ function start() {
 }
 
 function getData() {
-  // 스프레드시트의 ID와 범위를 고정해 놓았지만, 필요하면 수정할 수 있습니다.
-  var spreadsheetId = '1_5nQoggV38Y62T5JQfdOUL1RtxFaO6cdQFhh7IEsOlc';
-  var range = 'Sheet1!A1:A'; // 이 부분을 수정하여 원하는 시트 및 범위를 가져옵니다.
+  var range = 'Sheet1!A2:A'; // 이 부분을 수정하여 원하는 시트 및 범위를 가져옵니다.
 
   gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: spreadsheetId,
+    spreadsheetId: '1_5nQoggV38Y62T5JQfdOUL1RtxFaO6cdQFhh7IEsOlc',
     range: range
   }).then(function(response) {
     var numRows = response.result.values.length;
